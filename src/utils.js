f1f33192be470708db62dfa8ddd2e7480e2e5d26
medkov77 <medkov77@mail.ru>
2022-03-12 20:51:54 +0300

    add shape-module random color

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
 export function random(min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
+
+export function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
