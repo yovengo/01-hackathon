d30426bac240f78818c4fecd3c8f6972f1d4cd47
Nikolai200493 <Shaitor1993@mail.ru>
2022-03-13 16:13:02 +0300

    added clicks module

--- src/app.js
+++ src/app.js
@@ -5,15 +5,20 @@ import { ShapeModule } from './modules/shape.module'
 import { RandomSound } from '@/modules/random-sound.module'
 import { CustomMessage } from './modules/customMessage.module'
 import { BackgroundModule } from './modules/background.module'
+import { ClicksModule } from './modules/clicks.module'
 
 const contextMenu = new ContextMenu()
+
+// Creating modules
 const shapeModule = new ShapeModule()
 const randomSoundModule = new RandomSound()
 const customMessageModule = new CustomMessage()
 const backgroundModule = new BackgroundModule()
+const clicksModule = new ClicksModule()
 
-// добавляем свои модули тут через contextMenu.add(moduleName)
+// Adding modules: contextMenu.add(moduleName)
 contextMenu.add(shapeModule)
 contextMenu.add(randomSoundModule)
 contextMenu.add(customMessageModule)
 contextMenu.add(backgroundModule)
+contextMenu.add(clicksModule)
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,40 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
+  constructor() {
+    super('clicks', 'Считать клики за 3 секунды')
+    this.clickBlock = document.createElement('div')
+  }
 
+  trigger() {
+    const clickBlockTitle = document.createElement('h1')
+    clickBlockTitle.style.textAlign = 'center'
+
+    let timeRemains = 3
+    let clicksCount = -1
+
+    clickBlockTitle.innerHTML = `Кликайте по экрану<br>Оставшееся время: 3 секунд(ы)`
+    const timeInterval = setInterval(() => {
+      --timeRemains
+      clickBlockTitle.innerHTML = `Кликайте по экрану<br>Оставшееся время: ${timeRemains} секунд(ы)`
+      if (timeRemains === 0) {
+        alert(`Вы сделали ${clicksCount} кликов!`)
+        clearInterval(timeInterval)
+      }
+    }, 1000)
+
+    document.body.addEventListener('click', (event) => {
+      if (event) {
+        clicksCount += 1
+      }
+    })
+
+    setTimeout(() => {
+      clickBlockTitle.remove()
+      this.clickBlock.remove()
+    }, 3000)
+
+    this.clickBlock.append(clickBlockTitle)
+    document.body.append(this.clickBlock)
+  }
 }
