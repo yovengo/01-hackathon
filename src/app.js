fb1d9059f7ac6dd58ae899752f152ae94e067a61
Daniil Ognevoy <roxxor503@gmail.com>
2022-03-13 13:01:31 +0300

    added shape module to modules

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import "./styles.css";
 
 import { ContextMenu } from "./menu";
+import { ShapeModule } from "./modules/shape.module";
 
 const contextMenu = new ContextMenu();
+const shapeModule = new ShapeModule()
 
 // добавляем свои модули тут через contextMenu.add(moduleName)
+contextMenu.add(shapeModule)
